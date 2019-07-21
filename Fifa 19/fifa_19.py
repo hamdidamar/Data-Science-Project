356520f5994041274e12845ee3e0788de63f535c
Hamdi DAMAR <39102004+hamdidamar@users.noreply.github.com>
2019-07-21 23:05:47 +0300

    Update fifa_19.py

--- Fifa 19/fifa_19.py
+++ Fifa 19/fifa_19.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 fifa = pd.read_csv('fifa_19.csv') #kullanacağımız kütüphaneler datamızı import ettik
 
